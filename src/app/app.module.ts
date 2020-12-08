11179a80457886b7d5610c1983d5ede8baa9dc93
Praveen Kumar <praveensk.530@gmail.com>
2020-12-08 07:12:08 +0530

    Adding Angular files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ConfirmedComponent } from "./components/confirmed/confirmed.component";
+import { DeathComponent } from "./components/death/death.component";
+import { RecoveredComponent } from "./components/recovered/recovered.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CountrydropdownComponent } from "./components/countrydropdown/countrydropdown.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConfirmedComponent,
+    DeathComponent,
+    RecoveredComponent,
+    FooterComponent,
+    CountrydropdownComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
